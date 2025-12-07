798dce72afcca323381f7862df777d813fd20551
rudranilmajumder2006-jpg <rudranilmajumder2006@gmail.com>
2025-12-07 22:44:31 +0530

    Hacker-Rank day 3

---
+++ Sum of Two Numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+
+
+    int a,b;
+    scanf("%d",&a);
+    scanf("%d",&b);
+    
+    printf("The sum of %d and %d is %d.",a,b,a+b);
+    return 0;
+}
