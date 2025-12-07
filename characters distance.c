b6c45cfd059bf29301091c053de5480986a338ca
rudranilmajumder2006-jpg <rudranilmajumder2006@gmail.com>
2025-12-08 01:50:40 +0530

    Hacker rank day 4

---
+++ characters distance.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    char a;
+     char b;
+    scanf("%c %c",&a,&b);    
+    printf("The distance between %c and %c is %d",a,b,b-a);
+    
+    return 0;
+}
