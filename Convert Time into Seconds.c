70a11c339447c25ebe3216cf670031b406e9e77c
rudranilmajumder2006-jpg <rudranilmajumder2006@gmail.com>
2025-12-08 14:01:17 +0530

    Hacker Rank Day 5

---
+++ Convert Time into Seconds.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+
+   
+    int a,b,c;
+    scanf("%d %d %d",&a,&b,&c);
+    
+    printf("Total seconds: %d",a*3600+b*60+c);
+    return 0;
+}
