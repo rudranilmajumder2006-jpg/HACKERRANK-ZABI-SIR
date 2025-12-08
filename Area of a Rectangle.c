9925ead18a77e5bfc5e1e1a8dd710bff853c86f0
Rudranil <rudranilmajumder2006@gmail.com>
2025-12-09 00:14:17 +0530

    Hacker rank day 7

---
+++ Area of a Rectangle.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */    
+    int a,b;
+    scanf("%d",&a);
+    scanf("%d",&b);
+    printf("The area is: %d units",a*b);
+    return 0;
+}
