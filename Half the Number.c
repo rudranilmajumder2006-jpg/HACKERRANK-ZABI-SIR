925b1d7aedf8750be59e08b8b4d3e54fadf366db
Rudranil <rudranilmajumder2006@gmail.com>
2025-12-10 21:50:53 +0530

    hacker rank day 10

---
+++ Half the Number.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */    
+    int a;
+    scanf("%d",&a);
+    printf("Half of %d is %.2f",a,a/2.0);
+    return 0;
+}
