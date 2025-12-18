3c654f90f047420c8663559567893b788a4ab8fe
Rudranil <rudranilmajumder2006@gmail.com>
2025-12-18 21:45:03 +0530

    hacker rank day 20

---
+++ Rounded Value - Remove Decimals.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */  
+    float a;
+    int b;
+    scanf("%f",&a);
+    b=a;
+    printf("Rounded Value = %d",b);
+    
+    
+    return 0;
+}
