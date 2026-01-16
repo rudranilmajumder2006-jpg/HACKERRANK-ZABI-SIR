97ec205a6dc6613150a333d15c457abfed7980ac
Rudranil <rudranilmajumder2006@gmail.com>
2026-01-16 23:19:51 +0530

    hacker rank day 39

---
+++ Loops_Natural Numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */    
+    int N;
+    scanf("%d",&N);
+    
+    for(int i=1;i<=N;++i){
+        printf("%d ",i);
+    }
+    
+    return 0;
+}
