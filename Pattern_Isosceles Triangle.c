55abd8c39be8545b65d1d828dafe2899b58679cf
Rudranil <rudranilmajumder2006@gmail.com>
2026-01-25 01:48:12 +0530

    hacker rank day 49

---
+++ Pattern_Isosceles Triangle.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main() {
+
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */ 
+    
+    int n;
+    scanf("%d",&n);
+    
+    if(n<=1){
+        printf("Shape Not Possible");
+
+    }
+    
+    else{
+        for(int i=1;i<=n;i++){
+        for(int j=1;j<=n-i;j++){
+            printf(" ");
+        }
+        
+        for(int k=1;k<=2*i-1;k++){
+            printf("*");
+        }
+        
+        printf("\n");
+    }
+        
+        
+    }
+    
+    
+    
+    return 0;
+}
