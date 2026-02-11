fbb173593c7ab031dd7c95e49656afc6dd4e4b21
Rudranil <rudranilmajumder2006@gmail.com>
2026-02-11 09:23:43 +0530

    hacker rank day 64

---
+++ Loops_Sum of Multiples.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */ 
+   
+    
+    int sum=0, n;
+    scanf("%d",&n);
+    
+    
+    for(int i=1;i<=n;++i){
+        if(i%3==0){
+          sum +=i;
+        }
+        else if(i%5==0){
+           sum +=i;
+        }
+        else if(i%7==0){
+           sum +=i;
+        }
+    }
+    printf("%d",sum);
+    
+    return 0;
+}
