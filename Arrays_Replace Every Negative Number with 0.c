807fee4f78ccdd13aedf01ec9c1151a5fc98f731
Rudranil <rudranilmajumder2006@gmail.com>
2026-03-11 23:57:24 +0530

    hacker rank day 80

---
+++ Arrays_Replace Every Negative Number with 0.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */    
+    int n;
+    scanf("%d",&n);
+    int a[n];
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(int i=0;i<n;i++)
+    {
+        if(a[i]>=0)
+        {
+            printf("%d ",a[i]);
+        }
+        else{
+            printf("0 ");
+        }
+    }
+    return 0;
+}
